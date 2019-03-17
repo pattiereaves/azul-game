b9581042e81654705f15b4eb4b937901beaea88f
Pattie Reaves <pattie@alley.co>
2019-03-16 17:15:36 -0700

    Remove debug code.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-console.log(process.env.NODE_ENV === 'production');
-console.log(process.env.NODE_ENV);
-
 module.exports = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/azul-game' : '',
 };
