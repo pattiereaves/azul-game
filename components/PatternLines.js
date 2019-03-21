68cf85312789712a1f4c58fae763cfaed33805af
Pattie Reaves <pattie@alley.co>
2019-03-20 21:57:01 -0700

    Conditionally show button to place tiles.

---
+++ components/PatternLines.js
@@ -0,0 +1,61 @@
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'mineral-ui/Button';
+
+export default class FloorLines extends PureComponent {
+  static propTypes = {
+    lines: PropTypes.arrayOf(
+      PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.bool, PropTypes.number])),
+    ).isRequired,
+    tilesToPlace: PropTypes.arrayOf(PropTypes.number).isRequired,
+  };
+
+  /**
+   * Determines whether or not player can place tiles at this time.
+   *
+   * @param  {array} line   The line which will stage the tiles.
+   * @param  {array} tiles  Tiles to be placed.
+   * @return {bool}         Whether or not tiles can be placed.
+   */
+  canPlaceTilesInThisRow = (line, tiles) => {
+    // Are there tiles to place?
+    if (tiles.length < 1) {
+      return false;
+    }
+
+    // Is there any room in this row?
+    const emptyPlaces = line.filter(place => place === false);
+    if (emptyPlaces.length === 0) {
+      return false;
+    }
+
+    // If this row is empty than anything can be placed here.
+    if (emptyPlaces.length === line.length) {
+      return true;
+    }
+
+    // Do the tiles that are already in this row match the tiles to be placed?
+    const [tile] = tiles;
+    const [linetype] = line;
+    return tile === linetype;
+  }
+
+  render() {
+    const { lines, tilesToPlace } = this.props;
+    const { canPlaceTilesInThisRow } = this;
+    return (
+      <ul>
+        {lines.map(line => (
+          <li>
+            {canPlaceTilesInThisRow(line, tilesToPlace) && (
+              <Button primary>Place tiles in this row</Button>
+            )}
+            <ul>
+              {line.map(tile => (<li><Button>{tile}</Button></li>))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
